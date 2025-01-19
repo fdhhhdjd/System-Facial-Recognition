0ca8e3a0e5b43d563f8b301e0808dc3c3fbc1482
fdhhhdjd <nguyentientai10@gmail.com>
2025-01-19 16:08:23 +0700

    feat: #1 setup code

--- src/internal/handlers/error_handler.go
+++ src/internal/handlers/error_handler.go
@@ -1,1 +1,39 @@
 package handlers
+
+import (
+	"time"
+
+	pkg "facial-recognition/src/pkg/constants"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ErrorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Now     int64  `json:"now"`
+}
+
+func CreateErrorResponse(message string, status int, code int) *ErrorResponse {
+	return &ErrorResponse{
+		Code:    code,
+		Message: message,
+		Status:  status,
+		Now:     time.Now().Unix(),
+	}
+}
+
+// BadRequestError represents a 400 Bad Request error
+func BadRequestError(c *gin.Context, code int, messages ...string) {
+	message := ""
+	if len(messages) > 0 {
+		message = messages[0]
+	}
+
+	if message == "" {
+		message = pkg.GetReasonPhrase(pkg.StatusBadRequest)
+	}
+	response := CreateErrorResponse(message, pkg.StatusBadRequest, code)
+	Send(c, response.Status, response)
+}
